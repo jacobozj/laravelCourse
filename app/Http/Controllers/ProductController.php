629ad94396ccaef02014174e80fff692bdd4d551
Jacobo Zuluaga <jacobozuluaga80@gmail.com>
2024-08-21 14:46:33 -0500

    practice 2

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,69 +2,72 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
-use Illuminate\Http\RedirectResponse;
 
 class ProductController extends Controller
 {
-    public static $products = [
-        ["id"=>"1", "name"=>"TV", "description"=>"Best TV", "price"=>"1000"],
-        ["id"=>"2", "name"=>"iPhone", "description"=>"Best iPhone", "price"=>"2000"],
-        ["id"=>"3", "name"=>"Chromecast", "description"=>"Best Chromecast", "price"=>"3000"],
-        ["id"=>"4", "name"=>"Glasses", "description"=>"Best Glasses", "price"=>"4000"],
-    ];
+    // public static $products = [
+    //     ["id"=>"1", "name"=>"TV", "description"=>"Best TV", "price"=>"1000"],
+    //     ["id"=>"2", "name"=>"iPhone", "description"=>"Best iPhone", "price"=>"2000"],
+    //     ["id"=>"3", "name"=>"Chromecast", "description"=>"Best Chromecast", "price"=>"3000"],
+    //     ["id"=>"4", "name"=>"Glasses", "description"=>"Best Glasses", "price"=>"4000"],
+    // ];
 
     public function index(): View
     {
         $viewData = [];
-        $viewData["title"] = "Products - Online Store";
-        $viewData["subtitle"] =  "List of products";
-        $viewData["products"] = ProductController::$products;
-        
-        return view('product.index')->with("viewData", $viewData);
+        $viewData['title'] = 'Products - Online Store';
+        $viewData['subtitle'] = 'List of products';
+        $viewData['products'] = Product::all();
+
+        return view('product.index')->with('viewData', $viewData);
     }
 
-    public function show(string $id) : View | RedirectResponse
+    public function show(string $id): View|RedirectResponse
     {
         //isset checks if the value exists and is not null, then chekcs the value
         //of products array for the id
-        if(!isset(ProductController::$products[$id-1])){
-            //redirect to the index if isset is false
-            return redirect()->route("home.index");
-        }
+        // if(!isset(ProductController::$products[$id-1])){
+        //     //redirect to the index if isset is false
+        //     return redirect()->route("home.index");
+        // }
         $viewData = [];
-        $product = ProductController::$products[$id-1];
-        $viewData["title"] = $product["name"]." - Online Store";
-        $viewData["subtitle"] =  $product["name"]." - Product information";
-        $viewData["product"] = $product;
-        return view('product.show')->with("viewData", $viewData);
+        $product = Product::findOrFail($id);
+        $viewData['title'] = $product['name'].' - Online Store';
+        $viewData['subtitle'] = $product['name'].' - Product information';
+        $viewData['product'] = $product;
+
+        return view('product.show')->with('viewData', $viewData);
     }
-    public function create(): view | RedirectResponse
+
+    public function create(): view|RedirectResponse
     {
         $viewData = []; //to be sent to the view
-        $viewData["title"] = "Create product";
-        return view('product.create')->with("viewData",$viewData);
+        $viewData['title'] = 'Create product';
+
+        return view('product.create')->with('viewData', $viewData);
     }
-    public function save(Request $request)
+
+    public function save(Request $request): \Illuminate\Http\RedirectResponse
     {
         $request->validate([
-            "name" => "required",
-            "price" => "required"
+            'name' => 'required',
+            'price' => 'required',
         ]);
         // dd($request->all());
         //display message
-        // dd("Saved successfully!");
-        // return redirect()->route("product.created");
-        return view('product.product_created');
         //here will be the code to call the model and save it to the database
+        Product::create($request->only(['name', 'price']));
+
+        return redirect()->route('product.created');
+
     }
 
     public function created(): View
     {
         return view('product.product_created');
     }
-
 }
-
-
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
-Route::get('/about', 'App\Http\Controllers\HomeController@about')->name("home.about");
-Route::get('/contact', 'App\Http\Controllers\HomeController@contact')->name("contact.index");
-Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
+Route::get('/about', 'App\Http\Controllers\HomeController@about')->name('home.about');
+Route::get('/contact', 'App\Http\Controllers\HomeController@contact')->name('contact.index');
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name('product.index');
 // order matters -------------------------
-Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name("product.create");
-Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name("product.save");
+Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name('product.create');
+Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name('product.save');
 
 Route::get('/products/created', 'App\Http\Controllers\ProductController@created')->name('product.created');
 
-Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
-
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');
